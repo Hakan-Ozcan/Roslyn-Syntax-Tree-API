d06875621ef11d852c72bdab431f6877413c636e
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-02 17:43:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynSyntaxTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree Tree = CSharpSyntaxTree.ParseText(
+                            @"class OrnekClass
+                                {
+                                    public int OrnekProperty { get; set; }
+                                    public void OrnekMetod()
+                                    {
+ 
+                                    }
+                                    public void OrnekMetod(int x)
+                                    {
+ 
+                                    }
+ 
+                                    public void OrnekMetod2()
+                                    {
+ 
+                                    }
+                                }");
+            SyntaxNode Root = Tree.GetRoot();
+            var Methods = Root.DescendantNodes().OfType<MethodDeclarationSyntax>().Where(m=> !m.ParameterList.Parameters.Any());
+            var Propertys = Root.DescendantNodes().OfType<PropertyDeclarationSyntax>();
+            Console.WriteLine("Methods...");
+            foreach (var Method in Methods)
+            {
+                Console.WriteLine(Method.ToString());
+            }
+            Console.WriteLine("*********\nPropertys...");
+            foreach (var Property in Propertys)
+            {
+                Console.WriteLine(Property.ToString());
+            }
+
+
+            Console.Read();
+        }
+    }
+}
